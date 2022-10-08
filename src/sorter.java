cd220c01f0d4543a6d1b66b2abe37c9e6ab22e87
Abdullah-A-Alhabib <s202024640@kfupm.edu.sa>
2022-10-08 03:15:13 +0300

    Added Sort method

--- src/sorter.java
+++ src/sorter.java
@@ -1,6 +1,15 @@
-public class sorter {
+import java.util.*;
+
+public class Sorter {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-       
+        System.out.println("Hello World!")
+        Scanner read = new Scanner(System.in);
+        int[] list = new int[6];
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Enter number " + i+1);
+            int x = read.nextInt;
+            list[i] = x;
+        }
+        Collections.sort(list);
     }
 }
