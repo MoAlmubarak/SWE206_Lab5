ab736bfa2f1d946b8e16da32222c9625d8df6d45
Abdullah-A-Alhabib <s202024640@kfupm.edu.sa>
2022-10-08 05:25:38 +0300

    Added sort method

--- src/sorter.java
+++ src/sorter.java
@@ -3,20 +3,14 @@ import java.util.*;
 public class sorter {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        int[] list = new int[5];
-        list[0] = 5;
-        list[1] = 3;
-        list[2] = 1;
-        list[3] = 2;
-        list[4] = 4;
 
-        //Collections.sort(list);
         int [] numbers = new int[5];
         numbers[0] = 5;
         numbers[1] = 3;
         numbers[2] = 1;
         numbers[3] = 2;
         numbers[4] = 4;
+        //I think we should delete everything from here-
         selectionSort(numbers);
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
@@ -24,6 +18,7 @@ public class sorter {
         sortInsertion(numbers);
         for(int i=0; i< list.length; ++i){
             System.out.print(numbers[i] + " ");
+        //-to here. Note that it doesn't say that we have to use the sort() methods.
         }
         
     }
@@ -55,5 +50,10 @@ public class sorter {
             arr[i] = arr[min];
             arr[min] = temp;
         }
-    }   
+    }
+    public static void sort(int[] arr){ //Just copy this and replace the first line. (You will probably get an error because of functions with similar headers, I don't know what they were thinking with this question)
+        sortInsertion(arr);
+        for(int i=0; i< list.length; ++i)//CHANGE THIS TO i++ IN YOUR VERSION{
+            System.out.print(numbers[i] + " ");
+        }
     }
